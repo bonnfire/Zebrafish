d264972dd18f20533285b2dbd3811262dd363244
Bonnie Lin <b2lin@ucsd.edu>
2020-11-18 15:42:48 -0800

    extract and qc the (current and past) breeders for kn03

--- CREATE/CREATE_EXCEL.R
+++ CREATE/CREATE_EXCEL.R
@@ -18,3 +18,32 @@ Zebrafish_Guo_xl %>% get_dupes(fish_id) %>% subset(!is.na(ld_choice_index))%>% s
 
 # source("/home/bonnie/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/github/PalmerLab_genotyping/CREATE/EXTRACT_SAMPLEBARCODELIBRARY.R")
 # flowcell_df %>% subset(grepl("Plate", sample_id_demul)) %>% left_join(Zebrafish_Guo_xl, ., by = c("fish_id"= "rfid"))
+
+
+
+# submitted plates
+plates <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1NMMBpsPf4VDDckpZL2Qwrro5f3hqXpCTGGYYFKP4Hxc/edit#gid=1853834046")
+
+convertplate.todf <- function(x){
+  df_total <- data.frame()
+  for(i in seq(1, 36, by = 3)){
+    row <- x[i:(i+2)] 
+    df <- data.frame(row)
+    colnames(df) <- c("fish_id", "dna_conc", "num_larvae")
+    df_total <- rbind(df_total,df)
+  }
+  return(df_total)
+}
+
+plates_df_1 <- convertplate.todf(plates[6:13, c(2:17, 19:38)])
+
+
+# current breeders
+Zebrafish_breeders <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1NMMBpsPf4VDDckpZL2Qwrro5f3hqXpCTGGYYFKP4Hxc/edit#gid=1101471263')
+Zebrafish_breeders_df <- Zebrafish_breeders %>% 
+  clean_names %>% 
+  gather("sex", "rfid_genotype", -alternative_id, -x4, -x5, -x6) %>% 
+  mutate(rfid = gsub("(.*-\\D\\d+)\\D", "\\1", rfid_genotype)) %>%  # to join
+  mutate(rfid_id = gsub(".*-", "", rfid_genotype),
+         rfid_idstripped = gsub(".*-", "", rfid))
+
