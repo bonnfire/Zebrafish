caccca090b4d4ee776d79d723762e37643c39378
bonnfire <b2lin@health.ucsd.edu>
2021-03-11 15:33:51 -0800

    gh classification - to _

--- CREATE/CREATE_PEDIGREE.R
+++ CREATE/CREATE_PEDIGREE.R
@@ -1,4 +1,21 @@
 ## extract and process pedigree
+
+
+## process excel object from google sheets 
+pedigree_ggl <- Zebrafish_Guo_xl_wcleannames %>% 
+  subset(grepl("Plate", fish_id, ignore.case = T)) %>% 
+  distinct(fish_id, dna_collected_y_n, mother, father) %>% 
+  mutate_all(~gsub("-", "_", .)) %>% 
+  mutate(replace = ifelse(father %in% c("Z2622_M35", "Z2622_M34"), "yes", "no")) %>% 
+  mutate(replace = replace(replace, father == "Z2622_M35", "no"),
+    father = replace(father, father == "Z2622_M35", "Z2622_M34"),
+    father = replace(father, father == "Z2622_M34"&replace == "yes", "Z2622_M35")) %>% 
+  select(-replace)
+
+pedigree_ggl %>% write.csv("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/Zebrafish/csv files/pedigree_fish_n6580_20210114.csv", row.names = F)
+
+
+
 pedigree_fish <- read.csv("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/Zebrafish/pedigree_fish.csv") %>% 
   mutate_all(as.character) 
 
@@ -29,6 +46,8 @@ Zebrafish_Guo_xl %>%
           subset(grepl("guo", project_name)) %>% mutate(rfid = gsub("-", "_", rfid)) %>% select("fish_id" = rfid) %>% mutate(mother = "NA", father = "NA") %>% 
           subset(!(fish_id %in% gsub("-", "_", Zebrafish_Guo_xl$fish_id))),.) %>% write.csv("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/github/Zebrafish/CREATE/pedigree_fish_n6187_20210114.csv", row.names = F)
 
+read.csv("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/github/Zebrafish/CREATE/pedigree_fish_n6187_20210114.csv") %>% 
+  mutate_all(as.character)
 
 read.csv("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/github/PalmerLab_genotyping/CREATE/sample_barcode_lib_01052021.csv") %>% mutate_all(as.character) %>% subset(grepl("guo", project_name)) %>% mutate(rfid = gsub("-", "_", rfid)) %>% left_join(Zebrafish_Guo_xl %>% 
                                                                                                                                                                                                                                                           distinct(fish_id, mother, father) %>% 
